de3aa38ec6ac10b57a75578f23c46fbb95c3fbb6
Deepak Gullipalli <deepakgullipalli@yahoo.com>
2022-02-21 16:24:47 +0530

    example multiple expansion panel

--- src/app/multiple-expansion-panel/multiple-expansion-panel.component.ts
+++ src/app/multiple-expansion-panel/multiple-expansion-panel.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from "@angular/animations";
+import { SelectItem, PrimeNGConfig } from "primeng/api";
+import { CountryService } from "../../app/services/country.service";
+import { Console } from 'console';
 
 @Component({
   selector: 'app-multiple-expansion-panel',
@@ -7,18 +11,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MultipleExpansionPanelComponent implements OnInit {
 
-  items: TestExpansion[] = [];
-  constructor() { }
+  panelitems: TestExpansion[] = [];
+  selectedCities: string[] = [];
+  selectedCountries1: string[] = [];
+  selectedCountries2: string[] = [];
+  items: SelectItem[];
+  item: string;
+  cities: any[];
+  countries: any[];
+  constructor(private countryService: CountryService,
+    private primengConfig: PrimeNGConfig) { 
+    this.items = [];
+
+    this.countryService.getCountries().then(countries => {
+      this.items = countries;
+    });
+
+    this.countries = [
+      { name: "Australia", code: "AU" },
+      { name: "Brazil", code: "BR" },
+      { name: "China", code: "CN" },
+      { name: "Egypt", code: "EG" },
+      { name: "France", code: "FR" },
+      { name: "Germany", code: "DE" },
+      { name: "India", code: "IN" },
+      { name: "Japan", code: "JP" },
+      { name: "Spain", code: "ES" },
+      { name: "United States", code: "US" }
+    ];
+
+    this.cities = [
+      { name: "New York", code: "NY" },
+      { name: "Rome", code: "RM" },
+      { name: "London", code: "LDN" },
+      { name: "Istanbul", code: "IST" },
+      { name: "Paris", code: "PRS" }
+    ];
+  }
 
   ngOnInit(): void {
+    this.primengConfig.ripple = true;
   }
-  addPanel(){
-    this.items.push(new TestExpansion());
+  addPanel() {
+    this.panelitems.push(new TestExpansion());
+  }
+  savePanel(){
+    console.log(this.panelitems);
   }
 
 }
 
-export class TestExpansion{
+export class TestExpansion {
   Name: string;
-  Factor: any;
+  Cities: any[];
 }
